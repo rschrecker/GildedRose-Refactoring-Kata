f8156bfa482fc77260b9a4ff7e9389bc92f5d5e5
Rowan Schrecker <Rowan.Schrecker@softwire.com>
2017-11-16 11:33:21 +0000

    tested sulfuras

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -83,3 +83,14 @@ describe('Aged Brie', function () {
 });
 
 
+describe('Sulfuras', function() {
+
+    it('values do not change', function () {
+        const gildedRose = new Shop([new Item('Sulfuras, Hand of Ragnaros', 0, 80)]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].sellIn).toEqual(0);
+        expect(items[0].quality).toEqual(80);
+    });
+
+});
+
