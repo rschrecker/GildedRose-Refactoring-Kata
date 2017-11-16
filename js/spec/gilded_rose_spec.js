974b1c665c0bb791c4c4b645d13e2873e40e8d6c
Rowan Schrecker <Rowan.Schrecker@softwire.com>
2017-11-16 11:21:31 +0000

    tested brie

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -30,23 +30,56 @@ describe('Generic Item', function() {
         expect(items[1].quality).toEqual(54);
     });
 
-    it('decreases its quality faster when sellIn = 0', function () {
-        const gildedRose = new Shop([new Item('foo', 0, 3)]);
+    it('decreases its quality faster when sellIn <= 0', function () {
+        const gildedRose = new Shop([new Item('foo', 0, 3), new Item('foo', -5, 3)]);
         const items = gildedRose.updateQuality();
         expect(items[0].quality).toEqual(1);
-    });
-
-    it('decreases its quality faster when sellIn = 0', function () {
-        const gildedRose = new Shop([new Item('foo', 0, 3)]);
-        const items = gildedRose.updateQuality();
-        expect(items[0].quality).toEqual(1);
+        expect(items[1].quality).toEqual(1);
     });
 
     it('does not decreases in quality below 0', function () {
-        const gildedRose = new Shop([new Item('foo', -5, 1), new Item('foo2', 1, 5)]);
+        const gildedRose = new Shop([new Item('foo', -5, 1), new Item('foo2', 5, 0)]);
         const items = gildedRose.updateQuality();
         expect(items[0].quality).toEqual(0);
-        expect(items[0].quality).toEqual(0);
+        expect(items[1].quality).toEqual(0);
     });
 
 });
+
+
+describe('Aged Brie', function () {
+
+    it('decreases its sellIn value', function () {
+        const gildedRose = new Shop([new Item('Aged Brie', 1, 0)]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].sellIn).toEqual(0);
+    });
+
+    it('decreases its sellIn value below 0', function () {
+        const gildedRose = new Shop([new Item('Aged Brie', 0, 0)]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].sellIn).toEqual(-1);
+    });
+
+    it('increases its quality', function () {
+        const gildedRose = new Shop([new Item('Aged Brie', 1, 2)]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).toEqual(3);
+    });
+
+    it('increases its quality faster when sellIn <= 0', function () {
+        const gildedRose = new Shop([new Item('Aged Brie', 0, 2)]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).toEqual(4);
+    });
+
+    it('does not inrease its quality above 50', function () {
+        const gildedRose = new Shop([new Item('Aged Brie', 0, 49), new Item('Aged Brie', 0, 55)]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).toEqual(50);
+        expect(items[0].quality).toEqual(50);
+    });
+
+});
+
+
