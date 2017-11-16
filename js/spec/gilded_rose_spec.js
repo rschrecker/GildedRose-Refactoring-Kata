8c3e43f4710a78ba7946ddb1f18f6313bc9f5a0e
Rowan Schrecker <Rowan.Schrecker@softwire.com>
2017-11-16 10:31:40 +0000

    fixed indentation

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -1,9 +1,11 @@
-describe("Gilded Rose", function() {
+describe("Gilded Rose", function () {
 
-  it("should foo", function() {
-    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("fixme");
-  });
+    it("should foo", function () {
+        const gildedRose = new Shop([new Item("foo", 0, 0)]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].name).toEqual("foo");
+    });
 
 });
+
+
