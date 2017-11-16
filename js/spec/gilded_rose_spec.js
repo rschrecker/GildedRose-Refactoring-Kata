ebc9077ce34b49ef14219de52de919d6f558cd29
Rowan Schrecker <Rowan.Schrecker@softwire.com>
2017-11-16 11:01:58 +0000

    tested generic items

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -1,11 +1,52 @@
-describe("Gilded Rose", function () {
+describe('Gilded Rose', function () {
 
-    it("should foo", function () {
-        const gildedRose = new Shop([new Item("foo", 0, 0)]);
+    it('should foo', function () {
+        const gildedRose = new Shop([new Item('foo', 0, 0)]);
         const items = gildedRose.updateQuality();
-        expect(items[0].name).toEqual("foo");
+        expect(items[0].name).toEqual('foo');
     });
 
 });
 
 
+describe('Generic Item', function() {
+
+    it('decreases its sellIn value', function () {
+        const gildedRose = new Shop([new Item('foo', 1, 0)]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].sellIn).toEqual(0);
+    });
+
+    it('decreases its sellIn value below 0', function () {
+        const gildedRose = new Shop([new Item('foo', 0, 0)]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].sellIn).toEqual(-1);
+    });
+
+    it('decreases its quality', function () {
+        const gildedRose = new Shop([new Item('foo', 1, 2), new Item('foo2', 5, 55)]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).toEqual(1);
+        expect(items[1].quality).toEqual(54);
+    });
+
+    it('decreases its quality faster when sellIn = 0', function () {
+        const gildedRose = new Shop([new Item('foo', 0, 3)]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).toEqual(1);
+    });
+
+    it('decreases its quality faster when sellIn = 0', function () {
+        const gildedRose = new Shop([new Item('foo', 0, 3)]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).toEqual(1);
+    });
+
+    it('does not decreases in quality below 0', function () {
+        const gildedRose = new Shop([new Item('foo', -5, 1), new Item('foo2', 1, 5)]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).toEqual(0);
+        expect(items[0].quality).toEqual(0);
+    });
+
+});
